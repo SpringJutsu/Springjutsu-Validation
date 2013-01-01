e3736b6023edcb65daae28fc37b77eebddbeb26b
cduplichien <Clark.Duplichien@gmail.com>
2013-01-01 17:25:51 -0600

    Aside: build fix.

--- validation/src/test/java/org/springjutsu/validation/integrationTests/ValidationIntegrationTest.java
+++ validation/src/test/java/org/springjutsu/validation/integrationTests/ValidationIntegrationTest.java
@@ -1,30 +1,14 @@
 package org.springjutsu.validation.integrationTests;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.util.Locale;
-
 import org.junit.After;
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.validation.Errors;
 import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.webflow.execution.RequestContext;
-import org.springframework.webflow.test.MockExternalContext;
-import org.springframework.webflow.test.MockRequestContext;
 import org.springjutsu.validation.ValidationManager;
-import org.springjutsu.validation.test.entities.Address;
-import org.springjutsu.validation.test.entities.Customer;
-import org.springjutsu.validation.test.entities.ValuedCustomer;
 
-public class ValidationIntegrationTest {
+public abstract class ValidationIntegrationTest {
 	
 	protected static final String xmlDirectory = 
 		"org/springjutsu/validation/integration/";
